3032d299aeddcb2eb993a84490183c0963cd2fd6
Elixcz <109692427+Elixcz@users.noreply.github.com>
2022-10-01 20:45:37 +0200

    Removed worker from JS

--- js/script.js
+++ js/script.js
@@ -33,11 +33,3 @@ $(document).ready(function() {
 	});
 
 });
-
-if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('/sw.js').then(function(registration) {
-		console.log('ServiceWorker registration successful with scope: ', registration.scope);
-	}).catch(function(err) {
-    	console.log('ServiceWorker registration failed: ', err);
-    });
-}
