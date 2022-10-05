9cb20101f5518d588cf92cdafe2e58bcd8bbc103
Elixcz <109692427+Elixcz@users.noreply.github.com>
2022-10-05 20:02:07 +0200

    Added cover image in full-content-template

--- php/full-content-template.php
+++ php/full-content-template.php
@@ -3,6 +3,9 @@
     <div class="container pb-1">
         <div class="row">
             <div class="col-sm-12">
+				<?php if ( $page->coverImage() ): ?>
+					<img src="<?= $page->coverImage(); ?>" class="img-fluid rounded-top" alt="" width="2048" height="2048">
+                <?php endif ?>
                 <h1 class="h2 fw-bold mt-4 pb-2 text-center" id="post"><?= $page->title(); ?></h1>
             </div>
 
