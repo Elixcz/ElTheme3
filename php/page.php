9c985c5f6249cf1b8832ecb021a259d5761f6ea0
Elixcz <109692427+Elixcz@users.noreply.github.com>
2022-10-11 21:34:36 +0200

    Fix translate button

--- php/page.php
+++ php/page.php
@@ -13,7 +13,7 @@
                 <ul class="list-group list-group-flush">
                   <li class="list-group-item bg-dark text-secondary"><i class="bi bi-folder text-secondary"></i>&nbsp;<a href="<?= $page->categoryPermalink() ?>" title="<?= $page->category() ?>" rel="follow, noindex"><?= $page->category() ?></a></li>
                   <li class="list-group-item bg-dark text-secondary small"><i class="bi bi-clock text-secondary"></i>&nbsp;<?= $page->date(); ?></li>
-                  <li class="list-group-item bg-dark text-secondary translate-item"><i class="bi bi-translate text-secondary"></i>&nbsp;<a href="" id="translateBtn" title="Translate this page"><?= $L->get('Translate');?></a></li>
+                  <li class="list-group-item bg-dark text-secondary translate-item"><i class="bi bi-translate text-secondary"></i>&nbsp;<a href="#" id="translateBtn" title="Translate this page"><?= $L->get('Translate');?></a></li>
                 </ul>
             </div>
             <script defer type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateInit"></script>
